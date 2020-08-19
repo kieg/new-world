2273c6c8a5cc1190ac1daf09ab45419b73d29e19
Kie Gouveia <kie.gouveia@outlook.com>
2020-08-18 21:54:36 -0400

    Adding base file and map output.

---
+++ map.py
@@ -0,0 +1,28 @@
+import folium
+
+if __name__ == "__main__":
+    m = folium.Map(
+        location=[45.372, -121.6972],
+        zoom_start=12,
+        tiles='Stamen Terrain'
+    )
+
+    folium.Marker(
+        location=[45.3288, -121.6625],
+        popup='Mt. Hood Meadows',
+        icon=folium.Icon(icon='cloud')
+    ).add_to(m)
+
+    folium.Marker(
+        location=[45.3311, -121.7113],
+        popup='Timberline Lodge',
+        icon=folium.Icon(color='green')
+    ).add_to(m)
+
+    folium.Marker(
+        location=[45.3300, -121.6823],
+        popup='Some Other Location',
+        icon=folium.Icon(color='red', icon='info-sign')
+    ).add_to(m)
+
+    m.save('map.html')
