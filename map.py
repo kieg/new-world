7a9d9cd7e839ddffc70e41daf875f3b6697d1bc0
Kie Gouveia <kie.gouveia@outlook.com>
2020-08-22 22:41:29 -0400

    Changed to excel input. Added layers to media. Need to rename that...

--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 import folium
-from media import Media, Descriptions
+from media import Media, Descriptions, Layers
 import pandas as pd
 
 # Settings
@@ -7,46 +7,30 @@ icon_size = 40
 
 media = Media()
 desc = Descriptions()
+layers = Layers()
 
-# data = pd.read_csv('F:/Users/Kie/Documents/Projects/new_world')
+data = pd.read_csv('F:/Users/Kie/Documents/Projects/new_world/db.csv', header=0)
+
 
 class Mapping:
 
     def create_map(self):
         m = folium.Map(
-            location=[-11.5906166, 144.0332089],
-            zoom_start=3
+            location=[27.9715098, -29.7763845],
+            zoom_start=1.5
         )
 
-        layer_fauna = folium.FeatureGroup(name='Fauna')
-        layer_flora = folium.FeatureGroup(name='Flora')
-        layer_ecosystem = folium.FeatureGroup(name='Ecosystem')
-        layer_melt = folium.FeatureGroup(name='Melting Ice')
+        for index, row in data.iterrows():
+            folium.Marker(
+                location=[row['lat'], row['lon']],
+                popup=folium.Popup(getattr(desc, row["text"]), max_width=200),
+                icon=folium.features.CustomIcon(getattr(media, row['icon']), icon_size)
+            ).add_to(getattr(layers, row['layer']))
 
-
-        folium.Marker(
-            location=[-11.5906166, 144.0332089],
-            popup=folium.Popup(desc.desc_turtle, max_width=200),
-            icon=folium.features.CustomIcon(media.icon_url_turtle, icon_size)
-        ).add_to(layer_fauna)
-
-        folium.Marker(
-            location=[30.4416033, -91.1814585],
-            popup=folium.Popup(desc.desc_flood, max_width=200),
-            icon=folium.features.CustomIcon(media.icon_url_flood, icon_size)
-        ).add_to(layer_ecosystem)
-
-        folium.Marker(
-            location=[48.62305328822785, -113.76222200000001],
-            popup=folium.Popup(desc.desc_glacier_sperry, max_width=200),
-            icon=folium.features.CustomIcon(media.icon_url_glacier, icon_size)
-        ).add_to(layer_melt)
-
-
-        layer_fauna.add_to(m)
-        layer_flora.add_to(m)
-        layer_ecosystem.add_to(m)
-        layer_melt.add_to(m)
+        layers.layer_fauna.add_to(m)
+        layers.layer_flora.add_to(m)
+        layers.layer_ecosystem.add_to(m)
+        layers.layer_melt.add_to(m)
         folium.LayerControl().add_to(m)
 
         return m
